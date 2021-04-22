e79be3ea0578a5d66ade42296dca977fc574bc75
yero0214 <yero0214@gmail.com>
2021-04-23 00:03:55 +0900

    push

--- src/components/summoner/Tier.vue
+++ src/components/summoner/Tier.vue
@@ -5,6 +5,34 @@
             <div v-if="this.type == 1">자랭</div>
         </div>
         <div class="tier">
+            <div v-if="data.tier == 'IRON'">
+                <img src="@/assets/tier/Iron.png">
+            </div>
+            <div v-if="data.tier == 'BRONZE'">
+                <img src="@/assets/tier/Bronze.png">
+            </div>
+            <div v-if="data.tier == 'SILVER'">
+                <img src="@/assets/tier/Silver.png">
+            </div>
+            <div v-if="data.tier == 'GOLD'">
+                <img src="@/assets/tier/Gold.png">
+            </div>
+            <div v-if="data.tier == 'PLATINUM'">
+                <img src="@/assets/tier/Platinum.png">
+            </div>
+            <div v-if="data.tier == 'DIAMOND'">
+                <img src="@/assets/tier/Diamond.png">
+            </div>
+            <div v-if="data.tier == 'MASTER'">
+                <img src="@/assets/tier/Master.png">
+            </div>
+            <div v-if="data.tier == 'GRANDMASTER'">
+                <img src="@/assets/tier/Grandmaster.png">
+            </div>
+            <div v-if="data.tier == 'CHALLENGER'">
+                <img src="@/assets/tier/Challenger.png">
+            </div>
+
             {{data.tier}}&nbsp; {{data.rank}}
         </div>
         <div class="winlose">
@@ -37,5 +65,12 @@ export default {
     height:335px;
     border-radius: 0.4rem;
     color: black;
+    font-family: 'Gothic A1';
+}
+.back div{
+    font-size: 20px;
+}
+.tier img{
+    width:200px;
 }
 </style>
